614f55995494ee47da1a72d02193e1827c514531
Abdessamad <doughri.abdessamad@gmail.com>
2023-08-02 16:33:06 +0200

    Adding computer model
    - Adding computer model for sqlalchemy db
    -Empty main.py file

---
+++ app/computer_model.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Computer(Base):
+    __tablename__ = 'computers'
+
+    id = Column(Integer, primary_key=True, index=True)
+    mac_address = Column(String(17), unique=True, nullable=False)
+    computer_name = Column(String, nullable=False)
+    ip_address = Column(String(15), nullable=False) #IPv4 only 
+    employee_abbreviation = Column(String(3), nullable=True)
+    description = Column(String, nullable=True)
